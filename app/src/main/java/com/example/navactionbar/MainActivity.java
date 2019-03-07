a796cfa5bda7469cd705521baaaccb0053d960ff
Fabes1118 <35115141+Fabes1118@users.noreply.github.com>
2019-03-07 14:42:11 +0200

    string xml format

--- app/src/main/java/com/example/navactionbar/MainActivity.java
+++ app/src/main/java/com/example/navactionbar/MainActivity.java
@@ -114,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private class DrawerItemOnClickListerner implements ListView.OnItemClickListener{
-
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             selectItem(position);
